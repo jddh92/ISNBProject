02212ecfef1ba2cafe9d9fda262dc970e8fa369b
Juan <jddh92@gmail.com>
2024-05-25 01:26:55 -0500

    Clase para leer archivo. Historia de usuario 1.

---
+++ FileReaderClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISNBProject
+{
+    public class FileReaderClass
+    {
+        public async Task<List<string>> GetISBNList(string filePath)
+        {
+            List<string> isbnList = new List<string>();
+
+            try
+            {
+                StreamReader reader = new StreamReader(filePath);
+                {
+                    string line;
+                    while ((line = await reader.ReadLineAsync()) != null)
+                    {
+                        isbnList.AddRange(line.Split(',').Select(isbn => isbn.Trim()));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al leer el archivo: {ex.Message}");
+            }
+
+            return isbnList;
+        }
+    }
+}
