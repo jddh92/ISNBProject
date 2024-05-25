539d1cb164d4d105186100ed19da1e9330630f10
Juan <jddh92@gmail.com>
2024-05-25 02:17:23 -0500

    Clase para escribir el archivo  CSV. Método solución Historia de Usuario 5

---
+++ CSVWriter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISNBProject
+{
+    public class CSVWriter
+    {
+        public async Task WriteToCSV(string filePath, List<string[]> processedISBNs)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                StringBuilder csvContent = new StringBuilder();
+                csvContent.AppendLine("Row Number;Data Retrieval Type;ISBN;Title;Subtitle;Author Name(s);Number of Pages;Publish Date");
+
+                int rowNumber = 1;
+                foreach (string[] line in processedISBNs)
+                {
+                    csvContent.AppendLine($"{rowNumber};" + $"{string.Join(";", line.Select(item => item.Contains(";") ? $"\"{item}\"" : item))}");
+                    rowNumber++;
+                }
+
+                await writer.WriteAsync(csvContent.ToString());
+            }
+        }
+    }
+}
